4fc530167e6fe2c7cbe4278f9c0b10700e8f9b21
Mike Hepburn <eformat@gmail.com>
2024-07-12 21:41:38 +1000

    🦩 initial commit 🦩

--- chat.sh
+++ chat.sh
@@ -7,7 +7,7 @@ CMD=$1
 if [ -z "${CMD}" ] || [ "${CMD}" == "start" ]; then
 
   podman pod create --name=chat --share net -p 3001:3001 -p 8000:8000
-    
+
   podman create --name ilab --pod=chat --annotation run.oci.keep_original_groups=1 --name ilab --rm -it --entrypoint=bash  --device nvidia.com/gpu=0 --security-opt label=type:nvidia_container_t -v /home/mike/instructlab:/opt/app-root/src/instructlab:z -v /home/mike/.cache/huggingface:/opt/app-root/src/.cache/huggingface:z -v /home/mike/.local:/opt/app-root/src/.local:z quay.io/eformat/ilab-mike:latest
 
   podman create --name anythingllm --pod=chat --rm -it anythingllm \
