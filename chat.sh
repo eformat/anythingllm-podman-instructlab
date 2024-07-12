f66c3f3d9495318138dea9863c53667f07aec9d1
Mike Hepburn <eformat@gmail.com>
2024-07-12 21:41:02 +1000

    🦩 initial commit 🦩

--- chat.sh
+++ chat.sh
@@ -6,19 +6,23 @@ CMD=$1
 
 if [ -z "${CMD}" ] || [ "${CMD}" == "start" ]; then
 
-  podman run -d --name ilab --userns keep-id --annotation run.oci.keep_original_groups=1 --name ilab --rm -it --entrypoint=bash  --device nvidia.com/gpu=0 --security-opt label=type:nvidia_container_t -v /home/mike/instructlab:/opt/app-root/src/instructlab -v /home/mike/.cache/huggingface:/opt/app-root/src/.cache/huggingface --net=host quay.io/eformat/ilab-mike:latest
+  podman pod create --name=chat --share net -p 3001:3001 -p 8000:8000
+    
+  podman create --name ilab --pod=chat --annotation run.oci.keep_original_groups=1 --name ilab --rm -it --entrypoint=bash  --device nvidia.com/gpu=0 --security-opt label=type:nvidia_container_t -v /home/mike/instructlab:/opt/app-root/src/instructlab:z -v /home/mike/.cache/huggingface:/opt/app-root/src/.cache/huggingface:z -v /home/mike/.local:/opt/app-root/src/.local:z quay.io/eformat/ilab-mike:latest
 
-  podman exec ilab /bin/bash -c "cd instructlab; ilab serve &"
-
-  podman run -d --name anythingllm --rm -it anythingllm \
+  podman create --name anythingllm --pod=chat --rm -it anythingllm \
    -v ${STORAGE_LOCATION}:/app/server/storage:z \
    -v ${STORAGE_LOCATION}/.env:/app/server/.env:z \
    -e STORAGE_DIR="/app/server/storage" \
-   --net=host \
    mintplexlabs/anythingllm
+
+  podman start ilab
+  podman start anythingllm
+
+  podman exec ilab /bin/bash -c "cd instructlab; ilab serve &"  
 fi
 
 if [ "${CMD}" == "stop" ]; then
-    podman stop ilab
-    podman stop anythingllm
+    podman pod stop chat
+    podman pod rm chat
 fi
